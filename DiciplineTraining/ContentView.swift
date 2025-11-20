d5291205f6e7c9b58c657acfe756605d21fca753
Bjorn-Tore Almas <bjorn-tore@hosalmaas.no>
2025-11-20 19:13:27 +0100

    Initial Commit

---
+++ DiciplineTraining/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiciplineTraining
+//
+//  Created by Bjørn-Tore Almås on 20/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiciplineTraining/DiciplineTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiciplineTrainingApp.swift
+//  DiciplineTraining
+//
+//  Created by Bjørn-Tore Almås on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiciplineTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

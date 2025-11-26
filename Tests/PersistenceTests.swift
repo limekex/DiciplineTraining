f5b527d43b00a4457cd4dc38a2d5fb2af4773af5
Bjorn-Tore Almas <bjorn-tore@hosalmaas.no>
2025-11-26 09:54:21 +0100

    Added theme

--- Tests/PersistenceTests.swift
+++ Tests/PersistenceTests.swift
@@ -1,36 +1,237 @@
-//
-//  PersistenceTests.swift
-//  DisciplineTraining
-//
-//  Created by Bjørn-Tore Almås on 20/11/2025.
-//
+// !$*UTF8*$!
+{
+    archiveVersion = 1;
+    classes = {
+    };
+    objectVersion = 56;
+    objects = {
 
-import XCTest
-@testable import DisciplineTraining
+/* Begin PBXBuildFile section */
+        2E4E3E7D2CF34D3C00782A2D /* DisciplineTrainingApp.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2E4E3E7C2CF34D3C00782A2D /* DisciplineTrainingApp.swift */; };
+        2E4E3E7F2CF34D3C00782A2D /* ContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2E4E3E7E2CF34D3C00782A2D /* ContentView.swift */; };
+        2E4E3E822CF34D3D00782A2D /* Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = 2E4E3E812CF34D3D00782A2D /* Assets.xcassets */; };
+        2E4E3E852CF34D3D00782A2D /* Preview Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = 2E4E3E842CF34D3D00782A2D /* Preview Assets.xcassets */; };
+/* End PBXBuildFile section */
 
-class PersistenceTests: XCTestCase {
+/* Begin PBXFileReference section */
+        2E4E3E7B2CF34D3C00782A2D /* DisciplineTraining.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = DisciplineTraining.app; sourceTree = BUILT_PRODUCTS_DIR; };
+        2E4E3E7C2CF34D3C00782A2D /* DisciplineTrainingApp.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DisciplineTrainingApp.swift; sourceTree = "<group>"; };
+        2E4E3E7E2CF34D3C00782A2D /* ContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ContentView.swift; sourceTree = "<group>"; };
+        2E4E3E812CF34D3D00782A2D /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
+        2E4E3E842CF34D3D00782A2D /* Preview Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = "Preview Assets.xcassets"; sourceTree = "<group>"; };
+        2E4E3E872CF34D3D00782A2D /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
+/* End PBXFileReference section */
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+/* Begin PBXGroup section */
+        2E4E3E752CF34D3C00782A2D = {
+            isa = PBXGroup;
+            children = (
+                2E4E3E802CF34D3C00782A2D /* DisciplineTraining */,
+                2E4E3E7A2CF34D3C00782A2D /* Products */,
+            );
+            sourceTree = "<group>";
+        };
+        2E4E3E7A2CF34D3C00782A2D /* Products */ = {
+            isa = PBXGroup;
+            children = (
+                2E4E3E7B2CF34D3C00782A2D /* DisciplineTraining.app */,
+            );
+            name = Products;
+            sourceTree = "<group>";
+        };
+        2E4E3E802CF34D3C00782A2D /* DisciplineTraining */ = {
+            isa = PBXGroup;
+            children = (
+                2E4E3E7C2CF34D3C00782A2D /* DisciplineTrainingApp.swift */,
+                2E4E3E7E2CF34D3C00782A2D /* ContentView.swift */,
+                2E4E3E812CF34D3D00782A2D /* Assets.xcassets */,
+                2E4E3E832CF34D3D00782A2D /* Preview Content */,
+                2E4E3E872CF34D3D00782A2D /* Info.plist */,
+            );
+            path = DisciplineTraining;
+            sourceTree = "<group>";
+        };
+        2E4E3E832CF34D3D00782A2D /* Preview Content */ = {
+            isa = PBXGroup;
+            children = (
+                2E4E3E842CF34D3D00782A2D /* Preview Assets.xcassets */,
+            );
+            path = "Preview Content";
+            sourceTree = "<group>";
+        };
+/* End PBXGroup section */
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+/* Begin PBXNativeTarget section */
+        2E4E3E792CF34D3C00782A2D /* DisciplineTraining */ = {
+            isa = PBXNativeTarget;
+            buildConfigurationList = 2E4E3E8A2CF34D3D00782A2D /* Build configuration list for PBXNativeTarget "DisciplineTraining" */;
+            buildPhases = (
+                2E4E3E762CF34D3C00782A2D /* Sources */,
+                2E4E3E772CF34D3C00782A2D /* Frameworks */,
+                2E4E3E782CF34D3C00782A2D /* Resources */,
+            );
+            buildRules = (
+            );
+            dependencies = (
+            );
+            name = DisciplineTraining;
+            packageProductDependencies = (
+            );
+            productName = DisciplineTraining;
+            productReference = 2E4E3E7B2CF34D3C00782A2D /* DisciplineTraining.app */;
+            productType = "com.apple.product-type.application";
+        };
+/* End PBXNativeTarget section */
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check out https://developer.apple.com/documentation/xctest/defining_asynchronous_tests_in_xcode for more information.
-    }
+/* Begin PBXProject section */
+        2E4E3E732CF34D3C00782A2D /* Project object */ = {
+            isa = PBXProject;
+            attributes = {
+                LastSwiftUpdateCheck = 1520;
+                LastUpgradeCheck = 1520;
+                TargetAttributes = {
+                    2E4E3E792CF34D3C00782A2D = {
+                        CreatedOnToolsVersion = 15.2;
+                    };
+                };
+            };
+            buildConfigurationList = 2E4E3E882CF34D3D00782A2D /* Build configuration list for PBXProject "DisciplineTraining" */;
+            compatibilityVersion = "Xcode 14.0";
+            developmentRegion = en;
+            hasScannedForEncodings = 0;
+            knownRegions = (
+                en,
+                Base,
+            );
+            mainGroup = 2E4E3E752CF34D3C00782A2D;
+            productRefGroup = 2E4E3E7A2CF34D3C00782A2D /* Products */;
+            projectDirPath = "";
+            projectRoot = "";
+            targets = (
+                2E4E3E792CF34D3C00782A2D /* DisciplineTraining */,
+            );
+        };
+/* End PBXProject section */
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+/* Begin PBXSourcesBuildPhase section */
+        2E4E3E762CF34D3C00782A2D /* Sources */ = {
+            isa = PBXSourcesBuildPhase;
+            buildActionMask = 2147483647;
+            files = (
+                2E4E3E7D2CF34D3C00782A2D /* DisciplineTrainingApp.swift in Sources */,
+                2E4E3E7F2CF34D3C00782A2D /* ContentView.swift in Sources */,
+            );
+            runOnlyForDeploymentPostprocessing = 0;
+        };
+/* End PBXSourcesBuildPhase section */
 
+/* Begin PBXFrameworksBuildPhase section */
+        2E4E3E772CF34D3C00782A2D /* Frameworks */ = {
+            isa = PBXFrameworksBuildPhase;
+            buildActionMask = 2147483647;
+            files = (
+            );
+            runOnlyForDeploymentPostprocessing = 0;
+        };
+/* End PBXFrameworksBuildPhase section */
+
+/* Begin PBXResourcesBuildPhase section */
+        2E4E3E782CF34D3C00782A2D /* Resources */ = {
+            isa = PBXResourcesBuildPhase;
+            buildActionMask = 2147483647;
+            files = (
+                2E4E3E852CF34D3D00782A2D /* Preview Assets.xcassets in Resources */,
+                2E4E3E822CF34D3D00782A2D /* Assets.xcassets in Resources */,
+            );
+            runOnlyForDeploymentPostprocessing = 0;
+        };
+/* End PBXResourcesBuildPhase section */
+
+/* Begin XCConfigurationList section */
+        2E4E3E882CF34D3D00782A2D /* Build configuration list for PBXProject "DisciplineTraining" */ = {
+            isa = XCConfigurationList;
+            buildConfigurations = (
+                2E4E3E892CF34D3D00782A2D /* Debug */,
+                2E4E3E8B2CF34D3D00782A2D /* Release */,
+            );
+            defaultConfigurationIsVisible = 0;
+            defaultConfigurationName = Release;
+        };
+        2E4E3E8A2CF34D3D00782A2D /* Build configuration list for PBXNativeTarget "DisciplineTraining" */ = {
+            isa = XCConfigurationList;
+            buildConfigurations = (
+                2E4E3E8C2CF34D3D00782A2D /* Debug */,
+                2E4E3E8D2CF34D3D00782A2D /* Release */,
+            );
+            defaultConfigurationIsVisible = 0;
+            defaultConfigurationName = Release;
+        };
+/* End XCConfigurationList section */
+
+/* Begin XCBuildConfiguration section */
+        2E4E3E892CF34D3D00782A2D /* Debug */ = {
+            isa = XCBuildConfiguration;
+            buildSettings = {
+                ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
+                ASSETCATALOG_COMPILER_GLOBAL_ACCENT_COLOR_NAME = AccentColor;
+                CODE_SIGN_STYLE = Automatic;
+                CURRENT_PROJECT_VERSION = 1;
+                DEVELOPMENT_TEAM = "";
+                GENERATE_INFOPLIST_FILE = YES;
+                INFOPLIST_FILE = DisciplineTraining/Info.plist;
+                INFOPLIST_KEY_UIApplicationSupportsIndirectInputEvents = YES;
+                INFOPLIST_KEY_UILaunchScreen_Generation = YES;
+                IPHONEOS_DEPLOYMENT_TARGET = 17.2;
+                MARKETING_VERSION = 1.0;
+                PRODUCT_BUNDLE_IDENTIFIER = com.example.DisciplineTraining;
+                PRODUCT_NAME = "$(TARGET_NAME)";
+                SWIFT_EMIT_LOC_STRINGS = YES;
+                SWIFT_VERSION = 5.0;
+                TARGETED_DEVICE_FAMILY = "1,2";
+                ALWAYS_SEARCH_USER_PATHS = NO;
+                USE_HEADERMAP = NO;
+            };
+            name = Debug;
+        };
+        2E4E3E8B2CF34D3D00782A2D /* Release */ = {
+            isa = XCBuildConfiguration;
+            buildSettings = {
+                ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
+                ASSETCATALOG_COMPILER_GLOBAL_ACCENT_COLOR_NAME = AccentColor;
+                CODE_SIGN_STYLE = Automatic;
+                CURRENT_PROJECT_VERSION = 1;
+                DEVELOPMENT_TEAM = "";
+                GENERATE_INFOPLIST_FILE = YES;
+                INFOPLIST_FILE = DisciplineTraining/Info.plist;
+                INFOPLIST_KEY_UIApplicationSupportsIndirectInputEvents = YES;
+                INFOPLIST_KEY_UILaunchScreen_Generation = YES;
+                IPHONEOS_DEPLOYMENT_TARGET = 17.2;
+                MARKETING_VERSION = 1.0;
+                PRODUCT_BUNDLE_IDENTIFIER = com.example.DisciplineTraining;
+                PRODUCT_NAME = "$(TARGET_NAME)";
+                SWIFT_EMIT_LOC_STRINGS = YES;
+                SWIFT_VERSION = 5.0;
+                TARGETED_DEVICE_FAMILY = "1,2";
+            };
+            name = Release;
+        };
+        2E4E3E8C2CF34D3D00782A2D /* Debug */ = {
+            isa = XCBuildConfiguration;
+            buildSettings = {
+                DEVELOPMENT_ASSET_PATHS = "\"DisciplineTraining/Preview Content\"";
+                PRODUCT_NAME = "$(TARGET_NAME)";
+            };
+            name = Debug;
+        };
+        2E4E3E8D2CF34D3D00782A2D /* Release */ = {
+            isa = XCBuildConfiguration;
+            buildSettings = {
+                DEVELOPMENT_ASSET_PATHS = "\"DisciplineTraining/Preview Content\"";
+                PRODUCT_NAME = "$(TARGET_NAME)";
+            };
+            name = Release;
+        };
+/* End XCBuildConfiguration section */
+    };
+    rootObject = 2E4E3E732CF34D3C00782A2D /* Project object */;
 }
